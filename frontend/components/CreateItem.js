70d0348204c64e0642c8c5d74b9b05ee58fd90ef
Duncan Williams <duncanlw@gmail.com>
2020-04-07 12:29:19 +0300

    added update and delete items

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -41,6 +41,23 @@ export default class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "online-store");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dqn2euzsx/image/upload",
+      { method: "POST", body: data }
+    );
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -57,6 +74,25 @@ export default class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Title
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                ></input>
+                {this.state.image && (
+                  <img
+                    src={this.state.image}
+                    width="200px"
+                    alt="upload preview"
+                  />
+                )}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input

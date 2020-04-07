019349f1fd8949e39d4325df23bb952c3f0942b6
Duncan Williams <duncanlw@gmail.com>
2020-04-07 13:59:56 +0300

    added pagination

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -52,6 +52,7 @@ export default class CreateItem extends Component {
       { method: "POST", body: data }
     );
     const file = await res.json();
+    console.log(file);
     this.setState({
       image: file.secure_url,
       largeImage: file.eager[0].secure_url

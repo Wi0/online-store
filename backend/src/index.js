6d43f6571d45b3157cfe532a19186463b35f4c23
Duncan Williams <duncanlw@gmail.com>
2020-05-13 12:47:11 +0300

    added signup to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,23 @@
+const cookieParser = require("cookie-parser");
+
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
 
 const server = createServer();
 
+server.express.use(cookieParser());
+
+//TODO use express middleware to populate current user
+
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: process.env.FRONTEND_URL,
+    },
   },
-  details => {
+  (details) => {
     console.log(`Server is now running on port ${details.port}`);
   }
 );
---
+++ frontend/pages/signup.js
@@ -0,0 +1,9 @@
+const Signup = (props) => {
+  return (
+    <div>
+      <p>SIGNUP</p>
+    </div>
+  );
+};
+
+export default Signup;
